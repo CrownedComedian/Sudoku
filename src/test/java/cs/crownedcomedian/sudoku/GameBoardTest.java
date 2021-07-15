3da6ca065bea43a6b85a656d534e35f8c894e857
CrownedComedian <crownedcomedian@gmail.com>
2021-07-15 13:51:02 -0400

    added test GameBoard template.

---
+++ src/test/java/cs/crownedcomedian/sudoku/GameBoardTest.java
@@ -0,0 +1,41 @@
+package cs.crownedcomedian.sudoku;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameBoardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getSquare() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void getRow() {
+    }
+
+    @Test
+    void getCol() {
+    }
+
+    @Test
+    void getBox() {
+    }
+
+    @Test
+    void getAllof() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
